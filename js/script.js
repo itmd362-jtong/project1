2759c10f5a3fa107a1c98d1ab4209ee755d51f6a
jtong5 <jtongplus@gmail.com>
2025-03-05 19:41:14 -0600

    add a character counter

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 // adding this here so i know it's working
-console.log("javascript is loaded");
+console.log("Javascript is loaded");
 
 window.onload = function() {
   // change nojs to hasjs
-  document.documentElement.classList.remove('nojs');
-  document.documentElement.classList.add('hasjs');
+  document.documentElement.classList.remove("nojs");
+  document.documentElement.classList.add("hasjs");
   
   // get form
   var form = document.querySelector("form");
-  
+
   // add submit event handler
   if (form) {
     form.onsubmit = function(event) {
       event.preventDefault();
       
       // show message
-      alert("Thank you for signing up! You'll start receiving our weekly recipes soon!");
+      var name = document.getElementById("name").value;
+      alert("Thank you " + name + " for signing up! You'll start receiving our weekly recipes soon!");
       
       // reset the form
       form.reset();
       
       return false;
+    };
+  }
+  
+  // character counter for the textbox 
+  var allergiesTextarea = document.getElementById("allergies");
+  var charCount = document.getElementById("charCount");
+  
+  if (allergiesTextarea && charCount) {
+    allergiesTextarea.onkeyup = function() {
+      var remaining = 300 - allergiesTextarea.value.length;
+      charCount.textContent = remaining + " characters remaining"; // update the character counter
     };
   }
 };
