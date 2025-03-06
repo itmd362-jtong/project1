7c2daaa107af7e02e2465729835e66fcd94ca3c9
jtong5 <jtongplus@gmail.com>
2025-03-05 18:02:18 -0600

    i hope this fixes the javascript not working good

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('form');
-  
-  // add event listener for the submit button
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
+// adding this here so i know it's working
+console.log("javascript is loaded");
 
-    // pop up thank you message
-    alert("Thank you for signing up! You'll start receiving our weekly recipes soon.");
-    
-    form.reset();
-  });
-});
+window.onload = function() {
+  // change nojs to hasjs
+  document.documentElement.classList.remove('nojs');
+  document.documentElement.classList.add('hasjs');
+  
+  // get form
+  var form = document.querySelector("form");
+  
+  // add submit event handler
+  if (form) {
+    form.onsubmit = function(event) {
+      event.preventDefault();
+      
+      // show message
+      alert("Thank you for signing up! You'll start receiving our weekly recipes soon!");
+      
+      // reset the form
+      form.reset();
+      
+      return false;
+    };
+  }
+};
